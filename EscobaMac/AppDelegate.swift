0eee7222c5b0cb2fdae864e01c14fd96804cd40b
jcl <jlepore@itba.edu.ar>
2019-02-09 18:41:19 -0300

    Initial Commit

---
+++ EscobaMac/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  EscobaMac
+//
+//  Created by Juan Cruz Lepore on 9/2/19.
+//  Copyright © 2019 Juan Cruz Lepore. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
